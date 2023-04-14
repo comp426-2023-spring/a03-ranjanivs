b912c00871ac7e42ca8c44c1f0c24e28d99504fd
Your Name <you@example.com>
2023-04-13 23:24:46 -0400

    help message rpsls

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist'
-import { rps } from "/lib/rpsls.js"
+import minimist from "minimist"
+import { rps } from "../lib/rpsls.js"
+
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,43 @@
 #!/usr/bin/env node
+
+import minimist from "minimist"
+import { rpsls } from "../lib/rpsls.js"
+
+const argv = minimist(process.argv.slice(2))
+
+/*var help = 'Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}' */
+
+// usage message
+function helpMessage() {
+    console.log('Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}')
+    exit.process(0);
+}
+    
+// rules message
+function rulesMessage() {
+    console.log('')
+}
+    
+
+
+
